47d375af466b3b007be922d25241e14bc702b4b5
Alex Van Bussum <avanbussum@gmail.com>
2023-04-30 23:55:33 -0700

    vercel final

---
+++ .history/client/components/EventLog_20230430233224.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { IoMdCloseCircle } from 'react-icons/io'
+
+function EventLog({eventLog, setShowEventLog}) {
+  return (
+    <div className='bg-gray-200 rounded-md w-full px-5 pb-2 pt-4 mt-5'>
+        <div className='flex justify-between'>
+            <h1 className='font-bold text-2xl underline mb-4'> Event Log </h1>
+            <IoMdCloseCircle 
+                className='text-3xl cursor-pointer'
+                onClick={() => setShowEventLog(false)}
+            />
+        </div>
+        <ul>
+            {/* Loop through all patients here using the Patient component */}
+            {eventLog.map(item => (
+                <li key={item[1]._hex} className='font-bold bg-gray-300 p-4 mb-4 rounded-md'>
+                <p>Editor: {item[0]}</p>
+                <p>Patient ID: {item[1]._hex}</p>
+                <p>Patient Name: {item.firstName} {item.lastName}</p>
+                <p>Time: {new Date(parseInt(item.timestamp._hex,16)*1000).toLocaleString()}
+                </p>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default EventLog
---
+++ .history/client/components/TodoList_20230430233244.js
@@ -0,0 +1,76 @@
+import Navbar from './Navbar'
+import Patient from './Patient'
+import Modal from './Modal'
+import { IoMdAddCircle } from 'react-icons/io'
+import Link from 'next/link';
+import EventLog from './EventLog';
+import { useState } from 'react';
+
+
+
+const TodoList = ({eventLog, patients, firstName, lastName, setFirstName, setLastName, addPatient, deletePatient}) => 
+{
+  const [showEventLog, setShowEventLog] = useState(false)
+  return (
+  <div className='w-[70%] bg-[#354ea3] py-4 pb-8 px-9 rounded-[30px] h-fit'>
+    <Navbar />
+
+    <h2 className='text-4xl bolder text-white '>
+      Hello First Responder
+    </h2>
+    <div className='pt-3 text-[#7d99e9]'>PATIENTS ASSESSED</div>
+    <form className='flex items-center justify-center gap-3'>
+      <input
+        className='rounded-[10px] w-full p-[10px] border-none outline-none bg-[#031956] text-white mb-3'
+        placeholder='Patient First Name'
+        // take input from the form here
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <input
+        className='rounded-[10px] w-full p-[10px] border-none outline-none bg-[#031956] text-white mb-3'
+        placeholder='Patient Last Name'
+        // take input from the form here
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <IoMdAddCircle
+        // Add an onClick method
+        onClick={addPatient}
+        className='text-[#ea0aff] text-[80px] cursor-pointer ml-[22px] mb-[10px]'
+      />
+    </form>
+    <div className='overflow-y-scroll h-80'>
+      <ul>
+        {/* Loop through all patients here using the Patient component */}
+        {patients.map(item => (
+          <div key={item.id} className='p-4 pt-6 mb-4 bg-gray-200 rounded-md'>
+          <Patient
+            key={item.id}
+            firstName={item.firstName}
+            lastName={item.lastName}
+            onClick={deletePatient(item.id)}
+            //onClick delete
+          />    
+          </div>
+        ))}
+      </ul>
+    </div>
+    {showEventLog ? (
+        <EventLog
+          eventLog={eventLog}
+          setShowEventLog={setShowEventLog}
+        />
+      ) :
+        <button 
+          className='bg-gray-200 rounded-md w-full p-2 mt-5'
+          onClick={() => setShowEventLog(true)}
+        >
+          <h1 className='font-bold text-2xl underline mb-2'> Event Log </h1>
+        </button>
+    }
+  </div>
+  )
+}
+
+export default TodoList
